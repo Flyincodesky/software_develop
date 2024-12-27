8f3d6f041957a6a49680ea2da0e558be4474f247
Flyincodesky <2249607003@qq.com>
2024-12-27 14:08:57 +0800

    主要图形界面完成，文件目录展示切换等功能完成

---
+++ v1.3/include/window/fileExplor.hpp
@@ -0,0 +1,32 @@
+#ifndef FILEEXPLOR_HPP  // fileExplor.hpp
+#define FILEEXPLOR_HPP
+#include "../AllQtHead.hpp" //所有的Qt头文件
+
+extern QString viewDirPath;
+
+class FileExplorer : public QWidget {
+    Q_OBJECT
+ private:
+    
+    QLineEdit *pathInput;
+    QFileSystemModel *model;
+    QTreeView *treeView;
+    QLabel *cur_path_label;
+    QLabel *cur_selectedFile_label;
+
+ public:
+    QString  curViewPath;
+
+    FileExplorer(const QString &directoryPath , QWidget *parent = nullptr) ;  //  : QWidget(parent)
+    ~FileExplorer(){};
+ private slots:
+    void refresh(); // Enter按钮的点击函数, 确认输入的路径
+    void upDirPathView(); // Up按钮的点击函数 , 显示当前目录路径的上级目录
+
+    void onFileClicked(const QModelIndex &index) ; 
+    void onFileDoubleClicked(const QModelIndex &index);
+
+};
+
+#endif
+
